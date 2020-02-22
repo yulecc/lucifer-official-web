9265e3f40c284a9a1a34e1ba4320c997c4a427f3
CHANGYULE\Administrator <1184807635@qq.com>
2020-02-22 20:13:20 +0800

    优化目录结构

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import { asyncRouterMap } from '@/config/router.config'
+import { asyncRouterMap } from './router.config'
 
 Vue.use(VueRouter)
 
