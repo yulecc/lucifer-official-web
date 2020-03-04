b1c78bd7bbe0c25558c6a588f74d11010d23296a
CHANGYULE\Administrator <1184807635@qq.com>
2020-03-04 23:51:23 +0800

    feat(产品宫�功能模块): add nav of my new bookbook
    add nav of my new book and change nav font style
    
    fix #21

--- src/components/LayoutHeader/LayoutHeader.vue
+++ src/components/LayoutHeader/LayoutHeader.vue
@@ -6,18 +6,21 @@
       </router-link>
     </div>
     <nav>
-      <a-row type="flex" justify="start" align="middle">
+      <a-row class="nav-row" type="flex" justify="start" align="middle">
         <a-col :span="4">
-          <router-link to="/solution">题解</router-link>
+          <router-link to="/solution" class="nav-font">题解</router-link>
         </a-col>
         <a-col :span="4">
-          <router-link to="/everyday">每日一题</router-link>
+          <router-link to="/everyday" class="nav-font">每日一题</router-link>
         </a-col>
         <a-col :span="4">
-          <router-link to="/download">下载专区</router-link>
+          <router-link to="/download" class="nav-font">下载专区</router-link>
         </a-col>
         <a-col :span="4">
-          <router-link to="/video">视频专区</router-link>
+          <router-link to="/video" class="nav-font">视频专区</router-link>
+        </a-col>
+        <a-col :span="4" class="icon-new">
+          <router-link to="/video" class="nav-font">我的新书</router-link>
         </a-col>
       </a-row>
     </nav>
@@ -53,24 +56,35 @@ export default {
     display: flex;
     flex: 1;
     height: 100%;
-    font: 600 20px 'Google Sans Display', Arial, Helvetica, sans-serif;
-    text-shadow: 0px 0px 30px #00bcd4;
-    color: #122c30;
-    @keyframes hue {
-      0% {
-        filter: hue-rotate(0deg);
-      }
-      100% {
-        filter: hue-rotate(-360deg);
-      }
-    }
-    > div {
+    .nav-row {
       flex: 0 0 70%;
-      color: #156386;
-      background-image: linear-gradient(92deg, #eb5628 0%, #feab3a 100%);
-      animation: hue 100s infinite linear;
-      background-clip: text;
+      .nav-font {
+        color: #156386;
+        background-image: linear-gradient(92deg, #eb5628 0%, #feab3a 100%);
+        animation: hue 100s infinite linear;
+        background-clip: text;
+        font: 600 20px 'Google Sans Display', Arial, Helvetica, sans-serif;
+        text-shadow: 0px 0px 15px #00bcd4;
+        color: #156386;
+        @keyframes hue {
+          0% {
+            filter: hue-rotate(0deg);
+          }
+          100% {
+            filter: hue-rotate(-360deg);
+          }
+        }
+      }
     }
+  }
+  .icon-new::after {
+    content: 'new';
+    position: absolute;
+    top: -16px;
+    right: -8px;
+    font-size: 18px;
+    font-weight: bold;
+    color: red;
   }
 }
 </style>
