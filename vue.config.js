5fde331444c88a03c245ac28440bdaf519e7b30e
Joseph Jin <jerkjoe61@gmail.com>
2020-03-07 23:58:29 -0800

    public path

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,20 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const path = require('path')
+const path = require('path');
 function resolve(dir) {
-  return path.join(__dirname, dir)
+    return path.join(__dirname, dir);
 }
 
 module.exports = {
-  outputDir: 'docs',
-  css: {
-    loaderOptions: {
-      less: { javascriptEnabled: true }
+    publicPath:
+        process.env.NODE_ENV === 'production' ? '/lucifer-official-web/' : '/',
+    outputDir: 'docs',
+    css: {
+        loaderOptions: {
+            less: { javascriptEnabled: true }
+        }
+    },
+    chainWebpack: config => {
+        config.resolve.alias.set('@', resolve('src'));
+        config.devServer.disableHostCheck(true);
     }
-  },
-  chainWebpack: config => {
-    config.resolve.alias.set('@', resolve('src'))
-    config.devServer.disableHostCheck(true)
-  }
-}
+};
